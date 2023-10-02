34d972b4f1435435defb7822de45843736047f5d
sampath <118274231+sampath017@users.noreply.github.com>
2023-10-02 12:16:18 +0530

    Assignment: empty box pattern

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,15 @@
 public class Hello {
 	public static void main(String args[]) {
-		for (int i=1; i <= 6; i++) {
-			for (int j=1; j <= i; j++) {
-				System.out.print(j);
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= 4; j++) {
+				if (i == 1 || i == 4)
+					System.out.print("#");
+				else {
+					if (j == 1 || j == 4)
+						System.out.print("#");
+					else
+						System.out.print(" ");
+				}
 			}
 			System.out.println();
 		}
